c951226affd00e1841528c4937edbe71012c6e3f
SeongChan Ko <66778048+homechan77@users.noreply.github.com>
2021-05-11 13:42:37 +0900

    .

--- test0510.py
+++ test0510.py
@@ -1,1 +1,12 @@
-print("test0510")
+n,k = map(int, input().split())
+result = 0
+while True:
+    if n % k == 0:
+        n = n // k
+        result += 1
+    else:
+        n = n - 1
+        result += 1
+    if n == 1:
+        break
+print(result)
