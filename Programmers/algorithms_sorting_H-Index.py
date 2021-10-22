0ed0c0e4cae2c99a688b5d36dfd222bcd0273df8
SeongChan Ko <66778048+homechan77@users.noreply.github.com>
2021-10-22 19:50:49 +0900

    .

--- Programmers/algorithms_sorting_H-Index.py
+++ Programmers/algorithms_sorting_H-Index.py
@@ -8,10 +8,14 @@ def solution(citations):
             answer = j
             break
     
-    """
-    테스트케이스가 [9,9,9,12]와 같은 경우 반복문 중 조건문에 해당하는 경우가 없으므로 반복문을 빠져나오지 못한다. 따라서 for-else 구문을 활용하여 반복문 수행 종료시 반환 대상 객체 answer가 다음과 같은 값을 갖도록 한다.
-    """
+    
+    # 테스트케이스가 [9,9,9,12]와 같은 경우 반복문 중 조건문에 해당하는 경우가 없으므로 반복문을 빠져나오지 못한다.
+    # 따라서 for-else 구문을 활용하여 반복문 수행 종료시 반환 대상 객체 answer가 다음과 같은 값을 갖도록 한다.
+    
     else:
         answer = len(listx)
         
     return answer
+
+citations = [3, 0, 6, 1, 5]	
+print(solution(citations))
