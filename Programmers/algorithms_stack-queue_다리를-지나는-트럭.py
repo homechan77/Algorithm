d4d71f897310802f3eb9ee6342f99e1171610433
SeongChan Ko <66778048+homechan77@users.noreply.github.com>
2021-11-03 20:25:26 +0900

    .

--- Programmers/algorithms_stack-queue_다리를-지나는-트럭.py
+++ Programmers/algorithms_stack-queue_다리를-지나는-트럭.py
@@ -1,43 +1,62 @@
+#deque를 활용하지 않은 코드
+
 def solution(bridge_length, weight, truck_weights):
-    # answer = 0
-    
-    # count = 0
-    
-    
-    # removelist = []
-    # firsttruck = len(truck_weights)
+    q = [0] * bridge_length
+    sec = 0
 
-    # while truck_weights:
-    #     if len(truck_weights) == firsttruck:
-    #         removed1 = truck_weights.pop(0)
-    #         removelist.append(removed1)
-    #         count += 1
+    while q:
+        sec += 1
+        q.pop(0)
 
-    #     for 
-
-    count = 0        
-    arrived = []
-    going = []
-    firsttruck = len(truck_weights)
-    while truck_weights:
-        if len(truck_weights) == firsttruck:
-            going.append(truck_weights.pop(0))
-            count += 1
-
-
-        for _ in range(len())
-        if len(going) < bridge_length and sum(going) < weight:
-            if sum(sum(going), truck_weights[0]) < weight:
-                going.append(truck_weights.pop(0))
-                count += 1
+        if truck_weights:
+            if sum(q) + truck_weights[0] <= weight:
+                q.append(truck_weights.pop(0))
             else:
-                count += 1
-        else:
-            count += 1
-            going.clear()
+                q.append(0)
+    return sec
+
+##--------------------------------------------------------------------------##
+#deque를 활용한 코드
+
+from collections import deque
+
+def solution(bridge_length, weight, truck_weights):
+    
+    answer = 1
+    weights = deque([])
+    times = deque([])
+
+    truck = iter(truck_weights)
+    weights.append(next(truck, False))
+    times.append(0)
+    cur = next(truck, False)
+
+    while weight:
+        answer += 1
+        times = deque(t+1 for t in times)
+        
+        if times[0] == bridge_length:
+            times.popleft()
+            weight.popleft()
+
+        if cur and len(times)+1 <= bridge_length and sum(weights)+cur <= weight:
+            times.append(0)
+            weights.append(cur)
+            cur = next(truck, False)
+        
+        return answer
 
 
-    return count
+##--------------------------------------------------------------------------##
+#class의 활용
+
+import collections
+
+DUMMY_TRUCK = 0
+
+class Bridge(object):
+    def __init__(self, lenght, weight):
+
 
 
 bridge_length = 2
