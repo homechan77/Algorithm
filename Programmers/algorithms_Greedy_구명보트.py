af9ded133ba942d030848a7c42e1dfe5eb1c02a3
SeongChan Ko <66778048+homechan77@users.noreply.github.com>
2022-02-17 01:54:09 +0900

    .

--- Programmers/algorithms_Greedy_구명보트.py
+++ Programmers/algorithms_Greedy_구명보트.py
@@ -1,7 +1,8 @@
 # 모든 사람을 구출하기 위해 필요한 구명보트 개수의 최솟값을 return 
-# 한 번에 최대 2명씩 밖에 탈 수 없고...
+# 한 번에 최대 2명씩 밖에 탈 수 없고
 
-
+'''
+# 시도1 - 실패
 def solution(people, limit):
     people.sort(reverse=True)
     answer = []
@@ -31,9 +32,34 @@ def solution(people, limit):
         people.reverse()
         answer.append(templist)
             
+    return answer
+
+people = [70, 80, 50]
+limit = 100
+print(solution(people, limit))
+'''
+# 시도2 - 성공
+# tip1. 효율성 테스트를 통과하려면 반복문을 1번만 사용해야 한다.
+# tip2. pop() 함수를 사용하게 될 경우도 효율성 테스트에서 걸린다.
+
+from collections import deque
+
+def solution(people, limit):
+    people.sort()
+    people = deque(people)
+    answer = []
+    while people:
+        popn = people.pop()
+        if not people:
+            answer.append([popn])
+            break
+        elif popn + people[0] <= limit:
+            answer.append([people.popleft(), popn])
+        else:
+            answer.append([popn])
             
     return len(answer)
-
-people = [160, 150, 140, 60, 50, 40]
-limit = 200
+    
+people = [70, 80, 50]
+limit = 100
 print(solution(people, limit))
