ffa142c9bdccd45ff5e901a1b45dd76146cc25c8
SeongChan Ko <66778048+homechan77@users.noreply.github.com>
2022-05-30 11:05:24 +0900

    .

--- Programmers/algorithms_Greedy_조이스틱.py
+++ Programmers/algorithms_Greedy_조이스틱.py
@@ -1,4 +1,3 @@
-#문제 자체가 이해가 안됨;;
 """
 import string
 
@@ -33,28 +32,49 @@ def solution(name):
         if indexnum != 0:
 """
 
-def solution(name):
-    answer = 0
-    min_move = len(name) - 1
-    next = 0
+# def solution(name):
+#     answer = 0
+#     min_move = len(name) - 1
+#     next = 0
     
-    while name[min_move] == 'A' and min_move > 0:
-        min_move -= 1
+#     while name[min_move] == 'A' and min_move > 0:
+#         min_move -= 1
     
-    if (min_move < 0):
-        return answer
+#     if (min_move < 0):
+#         return answer
         
-    for i, char in enumerate(name):
-        answer += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
+#     for i, char in enumerate(name):
+#         answer += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
         
-        next = i + 1
-        while next < len(name) and name[next] == 'A':
-            next += 1
+#         next = i + 1
+#         while next < len(name) and name[next] == 'A':
+#             next += 1
         
-        min_move = min(min_move, i + (i + len(name)) - next)
-    answer += min_move
-    return answer
+#         min_move = min(min_move, i + (i + len(name)) - next)
+#     answer += min_move
+#     return answer
 
+
+def solution(name):
+    change = [min(ord(i)-ord('A'), ord('Z')-ord(i)+1) for i in name]
+    idx = 0
+    answer = 0
+    
+    while True:
+        answer += change[idx]
+        change[idx] =  0
+        if sum(change) == 0:
+            return answer
+        
+        left, right = 1, 1
+        while change[idx - left] == 0:
+            left += 1
+        while change[idx + right] == 0:
+            right += 1
+
+        answer += left if left < right else right
+        idx += left if left < right else right
 
 name = "JEROEN"
 print(solution(name))
+# return: 56
