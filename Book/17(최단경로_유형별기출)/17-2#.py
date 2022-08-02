918da880dc0e167f495f118f7fc007d2565f3575
SeongChan Ko <66778048+homechan77@users.noreply.github.com>
2022-08-02 20:28:29 +0900

    .

--- Book/17(최단경로_유형별기출)/17-2#.py
+++ Book/17(최단경로_유형별기출)/17-2#.py
@@ -9,8 +9,8 @@ array = [list(map(int, input().split())) for _ in range(m)]
 INF = int(1e9)
 
 def solution(n, array):
-    # 플로이드 워셜 알고리즘을 사용하여 두 노드간 직접적 연결 또는 간접적 연결 여부 상관없이 연결 현황을 파악한다.
-    # graph 안의 숫자는 i 노드와 j 노드가 몇 번 건너건너 연결되었는지를 알려준다. 
+    # 플로이드 워셜 알고리즘을 사용하여 두 노드간 직접적 연결 혹은 간접적 연결 여부 상관없이 연결 현황을 파악한다.
+    # graph 안의 숫자는 i노드(출발)와 j노드(도착)가 몇 번 건너건너 연결되었는지를 알려준다. 
     graph = [[INF for _ in range(n+1)] for _ in range(n+1)]
     for i in range(1, n+1):
         for j in range(1, n+1):
@@ -23,7 +23,7 @@ def solution(n, array):
             for j in range(1, n+1):
                 graph[i][j] = min(graph[i][j], graph[i][x]+graph[x][j])
     
-    # graph[i][j] 또는 graph[j][1]가 무한의 수가 아니라는 뜻은 i가 j보다 크거나 또는 작더라도 크기를 비교할 수 있다라는 뜻이기 때문에
+    # graph[i][j] 또는 graph[j][i]가 무한의 수가 아니라는 뜻은 i가 j보다 크거나 또는 작더라도 크기를 비교할 수 있다라는 뜻이기 때문에
     # 나머지 노드와 모두 비교 가능하다면 answer += 1
     answer = 0
     for i in range(1, n+1):
