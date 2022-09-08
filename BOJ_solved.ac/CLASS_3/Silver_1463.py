bf7e4c006020970980b1033546345a8b57dbe952
ko <homechan77@gmail.com>
2022-09-08 19:34:33 +0900

    .

--- BOJ_solved.ac/CLASS_3/Silver_1463.py
+++ BOJ_solved.ac/CLASS_3/Silver_1463.py
@@ -1,2 +1,28 @@
 # 1로 만들기
-print("change_vscode_workspace")
+
+# 시도1. 시간초과
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+n = int(input())
+
+def bfs(n):
+    queue = deque([[n, 0]])
+    while True:
+        pop = queue.popleft()
+        i, cnt = pop[0], pop[1]
+        if i == 1:
+            return cnt
+        if i % 3 == 0:
+            queue.append([i//3, cnt+1])
+        if i % 2 == 0:
+            queue.append([i//2, cnt+1])
+        queue.append([i-1, cnt+1])   
+
+def solution(n):
+    answer = bfs(n)
+    return answer
+
+print(solution(n))
+
