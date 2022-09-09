c9582d8cd2339e132cc8dc6a3e1baef2ef090a4d
ko <homechan77@gmail.com>
2022-09-09 19:04:14 +0900

    .

--- BOJ_solved.ac/CLASS_3/Silver_1463.java
+++ BOJ_solved.ac/CLASS_3/Silver_1463.java
@@ -1,9 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.lang.Math;
 
 public class Silver_1463 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] dp = new int[n+1];
+		for (int i=2; i<dp.length; i++) {
+			dp[i] = dp[i-1]+1;
+			if (i % 2 == 0) {
+				dp[i] = Math.min(dp[i], dp[i/2]+1);
+			}
+			if (i % 3 == 0) {
+				dp[i] = Math.min(dp[i], dp[i/3]+1);
+			}
+		}
+		System.out.println(dp[n]);
+		sc.close();
 	}
-
 }
